6ef23c34768c31552c8b3b679a3fa3a60adcc233
amit4049 <amitroysunjoy@gmail.com>
2021-03-23 12:16:58 +0600

    factorial

---
+++ factorial.rb
@@ -0,0 +1,14 @@
+puts "Enter the number:"
+num=gets.chomp.to_i
+
+fact=1
+if (num==0)
+	puts "Error! Could not find the factorial of one"
+else
+	i=1
+	while(i<=num)
+		fact=fact*i
+		i+=1
+	end
+end
+		puts "factorial of #{num} is #{fact}"
